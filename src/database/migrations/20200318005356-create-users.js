3711707fbab730271c7624aaca3066fd531918cd
MateusVS <mateusviniciussilva07@gmail.com>
2020-03-17 22:55:17 -0300

    Model e Migration de User

---
+++ src/database/migrations/20200318005356-create-users.js
@@ -0,0 +1,39 @@
+module.exports = {
+  up: (queryInterface, Sequelize) => queryInterface.createTable('Users', {
+    id: {
+      type: Sequelize.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+      allowNull: false,
+    },
+
+    name: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+
+    email: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+
+    password: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+
+    createdAt: {
+      type: Sequelize.DATE,
+      allowNull: false,
+    },
+
+    updatedAt: {
+      type: Sequelize.DATE,
+      allowNull: false,
+    },
+
+  }),
+
+  // eslint-disable-next-line no-unused-vars
+  down: (queryInterface, Sequelize) => queryInterface.dropTable('Users'),
+};
